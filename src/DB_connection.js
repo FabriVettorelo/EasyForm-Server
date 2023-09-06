4fc06d36af32fdf87b792d6b8c6b79bc426ca8cd
Fabrizio Vettorelo <114968631+FabriVettorelo@users.noreply.github.com>
2023-09-06 12:42:59 -0300

    Update DB_connection.js

--- src/DB_connection.js
+++ src/DB_connection.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_DEPLOY } = process.env;
 const FormModel = require("./models/Form");
 const UserModel = require("./models/User");
 const UserResponseModel = require("./models/UserResponse")
 const server = require('./app');
 
 const sequelize = new Sequelize(
-    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/forms`,
+    DB_DEPLOY,
    { logging: false, native: false }
 );
 FormModel(sequelize);
